4eb1c10a5ca7f5aa72f5cfac2d998a698335f154
Demian <demyan.korotchenko@gmail.com>
2023-08-10 10:24:51 +0200

    priiter settings

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -2,23 +2,29 @@ import PropTypes from 'prop-types';
 import { FriendListItem } from '../FriendListItem/FriendListItem';
 import { List } from './FriendList.styled';
 
-export const FriendList = ({friends}) => {
-    return (
-        <List> 
-            {friends.map((friend) => (
-          <FriendListItem key={friend.id} avatar={friend.avatar} 
-          name={friend.name} isOnline={friend.isOnline}/>
-          ))}
-        </List>
-    )
-}
+export const FriendList = ({ friends }) => {
+  return (
+    <List>
+      {friends.map(friend => (
+        <FriendListItem
+          key={friend.id}
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+        />
+      ))}
+    </List>
+  );
+};
 
 FriendList.propTypes = {
-    friends: PropTypes.arrayOf(PropTypes.shape({
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
       id: PropTypes.number.isRequired,
       avatar: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
       isOnline: PropTypes.bool.isRequired,
-    }).isRequired).isRequired
-    
-    }
+    }).isRequired
+  ).isRequired,
+};
+
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,67 +1,56 @@
-import propTypes from "prop-types";
-
+import propTypes from 'prop-types';
 
 import {
-    ProfileCard,
-    Description,
-    Avatar,
-    Name,
-    Tag,
-    Location,
-    StatsList,
-    StatsItem,
-    Label,
-    Quantity,
-  } from './Profile.styled';
+  ProfileCard,
+  Description,
+  Avatar,
+  Name,
+  Tag,
+  Location,
+  StatsList,
+  StatsItem,
+  Label,
+  Quantity,
+} from './Profile.styled';
 
-
-
-export const Profile = ({username, tag, location, avatar, stats}) => {
-    return (
-   
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
     <ProfileCard>
-    <Description>
-       <Avatar src={avatar} alt="User avatar"/>
-       <Name>{username}</Name>
-       <Tag>@{tag}</Tag>
-       <Location>{location}</Location>
-   </Description>
+      <Description>
+        <Avatar src={avatar} alt="User avatar" />
+        <Name>{username}</Name>
+        <Tag>@{tag}</Tag>
+        <Location>{location}</Location>
+      </Description>
 
-   <StatsList>
+      <StatsList>
+        <StatsItem>
+          <Label>Followers</Label>
+          <Quantity>{stats.followers}</Quantity>
+        </StatsItem>
 
-       <StatsItem>
-           <Label>Followers</Label>
-           <Quantity>{stats.followers}</Quantity>
-       </StatsItem>
+        <StatsItem>
+          <Label>Views</Label>
+          <Quantity>{stats.views}</Quantity>
+        </StatsItem>
 
-       <StatsItem>
-           <Label>Views</Label>
-           <Quantity>{stats.views}</Quantity>
-       </StatsItem>
-
-       <StatsItem>
-           <Label>Likes</Label>
-           <Quantity>{stats.likes}</Quantity>
-       </StatsItem>
-
-   </StatsList>
-
-   </ProfileCard>
-
-
-    );
-} ;
-
-
+        <StatsItem>
+          <Label>Likes</Label>
+          <Quantity>{stats.likes}</Quantity>
+        </StatsItem>
+      </StatsList>
+    </ProfileCard>
+  );
+};
 
 Profile.propTypes = {
-    username: propTypes.string.isRequired,
-    tag: propTypes.string.isRequired, 
-    location: propTypes.string.isRequired, 
-    avatar: propTypes.string.isRequired, 
-    stats: propTypes.shape({
-        followers: propTypes.number.isRequired,
-        views: propTypes.number.isRequired,
-        likes: propTypes.number.isRequired
-      }).isRequired,
-} 
+  username: propTypes.string.isRequired,
+  tag: propTypes.string.isRequired,
+  location: propTypes.string.isRequired,
+  avatar: propTypes.string.isRequired,
+  stats: propTypes.shape({
+    followers: propTypes.number.isRequired,
+    views: propTypes.number.isRequired,
+    likes: propTypes.number.isRequired,
+  }).isRequired,
+};
